d252df439bdda73c01dce9be9ef23b404926e025
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2022-04-27 18:11:48 +0200

    Fixed breadcrumb names for updated routes

--- src/resources/routes/breadcrumbs.php
+++ src/resources/routes/breadcrumbs.php
@@ -2,112 +2,112 @@
 
 declare(strict_types=1);
 
-Breadcrumbs::for('vanilo.product.index', function ($breadcrumbs) {
+Breadcrumbs::for('vanilo.admin.product.index', function ($breadcrumbs) {
     $breadcrumbs->parent('home');
-    $breadcrumbs->push(__('Products'), route('vanilo.product.index'));
+    $breadcrumbs->push(__('Products'), route('vanilo.admin.product.index'));
 });
 
-Breadcrumbs::for('vanilo.product.create', function ($breadcrumbs) {
-    $breadcrumbs->parent('vanilo.product.index');
+Breadcrumbs::for('vanilo.admin.product.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('vanilo.admin.product.index');
     $breadcrumbs->push(__('Create'));
 });
 
-Breadcrumbs::for('vanilo.product.show', function ($breadcrumbs, $product) {
-    $breadcrumbs->parent('vanilo.product.index');
-    $breadcrumbs->push($product->name, route('vanilo.product.show', $product));
+Breadcrumbs::for('vanilo.admin.product.show', function ($breadcrumbs, $product) {
+    $breadcrumbs->parent('vanilo.admin.product.index');
+    $breadcrumbs->push($product->name, route('vanilo.admin.product.show', $product));
 });
 
-Breadcrumbs::for('vanilo.product.edit', function ($breadcrumbs, $product) {
-    $breadcrumbs->parent('vanilo.product.show', $product);
-    $breadcrumbs->push(__('Edit'), route('vanilo.product.edit', $product));
+Breadcrumbs::for('vanilo.admin.product.edit', function ($breadcrumbs, $product) {
+    $breadcrumbs->parent('vanilo.admin.product.show', $product);
+    $breadcrumbs->push(__('Edit'), route('vanilo.admin.product.edit', $product));
 });
 
-Breadcrumbs::for('vanilo.taxonomy.index', function ($breadcrumbs) {
+Breadcrumbs::for('vanilo.admin.taxonomy.index', function ($breadcrumbs) {
     $breadcrumbs->parent('home');
-    $breadcrumbs->push(__('Categorization'), route('vanilo.taxonomy.index'));
+    $breadcrumbs->push(__('Categorization'), route('vanilo.admin.taxonomy.index'));
 });
 
-Breadcrumbs::for('vanilo.taxonomy.create', function ($breadcrumbs) {
-    $breadcrumbs->parent('vanilo.taxonomy.index');
+Breadcrumbs::for('vanilo.admin.taxonomy.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('vanilo.admin.taxonomy.index');
     $breadcrumbs->push(__('Create'));
 });
 
-Breadcrumbs::for('vanilo.taxonomy.show', function ($breadcrumbs, $taxonomy) {
-    $breadcrumbs->parent('vanilo.taxonomy.index');
-    $breadcrumbs->push($taxonomy->name, route('vanilo.taxonomy.show', $taxonomy));
+Breadcrumbs::for('vanilo.admin.taxonomy.show', function ($breadcrumbs, $taxonomy) {
+    $breadcrumbs->parent('vanilo.admin.taxonomy.index');
+    $breadcrumbs->push($taxonomy->name, route('vanilo.admin.taxonomy.show', $taxonomy));
 });
 
-Breadcrumbs::for('vanilo.taxonomy.edit', function ($breadcrumbs, $taxonomy) {
-    $breadcrumbs->parent('vanilo.taxonomy.show', $taxonomy);
-    $breadcrumbs->push(__('Edit'), route('vanilo.taxonomy.edit', $taxonomy));
+Breadcrumbs::for('vanilo.admin.taxonomy.edit', function ($breadcrumbs, $taxonomy) {
+    $breadcrumbs->parent('vanilo.admin.taxonomy.show', $taxonomy);
+    $breadcrumbs->push(__('Edit'), route('vanilo.admin.taxonomy.edit', $taxonomy));
 });
 
-Breadcrumbs::for('vanilo.taxon.create', function ($breadcrumbs, $taxonomy) {
-    $breadcrumbs->parent('vanilo.taxonomy.show', $taxonomy);
+Breadcrumbs::for('vanilo.admin.taxon.create', function ($breadcrumbs, $taxonomy) {
+    $breadcrumbs->parent('vanilo.admin.taxonomy.show', $taxonomy);
     $breadcrumbs->push(__('Create'));
 });
 
-Breadcrumbs::for('vanilo.taxon.edit', function ($breadcrumbs, $taxonomy, $taxon) {
-    $breadcrumbs->parent('vanilo.taxonomy.show', $taxonomy);
+Breadcrumbs::for('vanilo.admin.taxon.edit', function ($breadcrumbs, $taxonomy, $taxon) {
+    $breadcrumbs->parent('vanilo.admin.taxonomy.show', $taxonomy);
     $breadcrumbs->push($taxon->name);
 });
 
-Breadcrumbs::for('vanilo.order.index', function ($breadcrumbs) {
+Breadcrumbs::for('vanilo.admin.order.index', function ($breadcrumbs) {
     $breadcrumbs->parent('home');
-    $breadcrumbs->push(__('Orders'), route('vanilo.order.index'));
+    $breadcrumbs->push(__('Orders'), route('vanilo.admin.order.index'));
 });
 
-Breadcrumbs::for('vanilo.order.show', function ($breadcrumbs, $order) {
-    $breadcrumbs->parent('vanilo.order.index');
-    $breadcrumbs->push($order->getNumber(), route('vanilo.order.show', $order));
+Breadcrumbs::for('vanilo.admin.order.show', function ($breadcrumbs, $order) {
+    $breadcrumbs->parent('vanilo.admin.order.index');
+    $breadcrumbs->push($order->getNumber(), route('vanilo.admin.order.show', $order));
 });
 
-Breadcrumbs::for('vanilo.property.index', function ($breadcrumbs) {
+Breadcrumbs::for('vanilo.admin.property.index', function ($breadcrumbs) {
     $breadcrumbs->parent('home');
-    $breadcrumbs->push(__('Properties'), route('vanilo.property.index'));
+    $breadcrumbs->push(__('Properties'), route('vanilo.admin.property.index'));
 });
 
-Breadcrumbs::for('vanilo.property.create', function ($breadcrumbs) {
-    $breadcrumbs->parent('vanilo.property.index');
+Breadcrumbs::for('vanilo.admin.property.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('vanilo.admin.property.index');
     $breadcrumbs->push(__('Create'));
 });
 
-Breadcrumbs::for('vanilo.property.show', function ($breadcrumbs, $property) {
-    $breadcrumbs->parent('vanilo.property.index');
-    $breadcrumbs->push($property->name, route('vanilo.property.show', $property));
+Breadcrumbs::for('vanilo.admin.property.show', function ($breadcrumbs, $property) {
+    $breadcrumbs->parent('vanilo.admin.property.index');
+    $breadcrumbs->push($property->name, route('vanilo.admin.property.show', $property));
 });
 
-Breadcrumbs::for('vanilo.property.edit', function ($breadcrumbs, $property) {
-    $breadcrumbs->parent('vanilo.property.show', $property);
-    $breadcrumbs->push(__('Edit'), route('vanilo.property.edit', $property));
+Breadcrumbs::for('vanilo.admin.property.edit', function ($breadcrumbs, $property) {
+    $breadcrumbs->parent('vanilo.admin.property.show', $property);
+    $breadcrumbs->push(__('Edit'), route('vanilo.admin.property.edit', $property));
 });
 
-Breadcrumbs::for('vanilo.property_value.create', function ($breadcrumbs, $property) {
-    $breadcrumbs->parent('vanilo.property.show', $property);
+Breadcrumbs::for('vanilo.admin.property_value.create', function ($breadcrumbs, $property) {
+    $breadcrumbs->parent('vanilo.admin.property.show', $property);
     $breadcrumbs->push(__('Create Value'));
 });
 
-Breadcrumbs::for('vanilo.property_value.edit', function ($breadcrumbs, $property, $propertyValue) {
-    $breadcrumbs->parent('vanilo.property.show', $property);
+Breadcrumbs::for('vanilo.admin.property_value.edit', function ($breadcrumbs, $property, $propertyValue) {
+    $breadcrumbs->parent('vanilo.admin.property.show', $property);
     $breadcrumbs->push($propertyValue->title);
 });
 
-Breadcrumbs::for('vanilo.channel.index', function ($breadcrumbs) {
+Breadcrumbs::for('vanilo.admin.channel.index', function ($breadcrumbs) {
     $breadcrumbs->parent('home');
-    $breadcrumbs->push(__('Channels'), route('vanilo.channel.index'));
+    $breadcrumbs->push(__('Channels'), route('vanilo.admin.channel.index'));
 });
 
-Breadcrumbs::for('vanilo.channel.create', function ($breadcrumbs) {
-    $breadcrumbs->parent('vanilo.channel.index');
+Breadcrumbs::for('vanilo.admin.channel.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('vanilo.admin.channel.index');
     $breadcrumbs->push(__('Create'));
 });
 
-Breadcrumbs::for('vanilo.channel.show', function ($breadcrumbs, $channel) {
-    $breadcrumbs->parent('vanilo.channel.index');
-    $breadcrumbs->push($channel->name, route('vanilo.channel.show', $channel));
+Breadcrumbs::for('vanilo.admin.channel.show', function ($breadcrumbs, $channel) {
+    $breadcrumbs->parent('vanilo.admin.channel.index');
+    $breadcrumbs->push($channel->name, route('vanilo.admin.channel.show', $channel));
 });
 
-Breadcrumbs::for('vanilo.channel.edit', function ($breadcrumbs, $channel) {
-    $breadcrumbs->parent('vanilo.channel.show', $channel);
-    $breadcrumbs->push(__('Edit'), route('vanilo.channel.edit', $channel));
+Breadcrumbs::for('vanilo.admin.channel.edit', function ($breadcrumbs, $channel) {
+    $breadcrumbs->parent('vanilo.admin.channel.show', $channel);
+    $breadcrumbs->push(__('Edit'), route('vanilo.admin.channel.edit', $channel));
 });
