6baea67c507f01dd16db2e5f4b078343265ced1b
Levente Malnasi <lte@vanilo.io>
2023-06-15 21:23:25 +0300

    Added properties buttons to the page-actions

--- src/resources/views/property/show.blade.php
+++ src/resources/views/property/show.blade.php
@@ -4,28 +4,28 @@
     {{ $property->name }} {{ __('property') }}
 @stop
 
+@push('page-actions')
+    @can('delete properties')
+        {!! Form::open([
+                'route' => ['vanilo.admin.property.destroy', $property],
+                'method' => 'DELETE',
+                'class' => 'd-inline',
+                'data-confirmation-text' => __('Delete this property: ":name"?', ['name' => $property->name])
+            ])
+        !!}
+        <x-appshell::button variant="outline-danger" type="submit">{{ __('Delete Property') }}</x-appshell::button>
+        {!! Form::close() !!}
+    @endcan
+
+    @can('edit properties')
+        <x-appshell::button :href="route('vanilo.admin.property.edit', $property)" variant="outline-primary">{{ __('Edit Property') }}</x-appshell::button>
+    @endcan
+@endpush
+
 @section('content')
     <x-appshell::card accent="secondary">
         <x-slot:title>{{ __(':name Values', ['name' => $property->name]) }}</x-slot:title>
 
         @include('vanilo::property-value._index', ['propertyValues' => $property->values()])
-
-        <x-slot:footer>
-            @can('edit properties')
-                <x-appshell::button :href="route('vanilo.admin.property.edit', $property)" variant="outline-primary">{{ __('Edit Property') }}</x-appshell::button>
-            @endcan
-
-            @can('delete properties')
-                {!! Form::open([
-                        'route' => ['vanilo.admin.property.destroy', $property],
-                        'method' => 'DELETE',
-                        'class' => 'd-inline',
-                        'data-confirmation-text' => __('Delete this property: ":name"?', ['name' => $property->name])
-                    ])
-                !!}
-                <x-appshell::button variant="outline-danger" type="submit">{{ __('Delete Property') }}</x-appshell::button>
-                {!! Form::close() !!}
-            @endcan
-        </x-slot:footer>
     </x-appshell::card>
 @stop
