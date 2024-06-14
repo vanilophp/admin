6db44ed3d3056dfebf3c00485c0731db3a9d1c56
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2024-06-14 16:46:37 +0300

    Aki hülye, az is marad

--- src/resources/views/link/_group.blade.php
+++ src/resources/views/link/_group.blade.php
@@ -2,10 +2,13 @@
     <h6>{{ __(':type items of :root', ['type' => $group->type->name, 'root' => $group->rootItem->linkable->name]) }}</h6>
     @foreach($group->items as $link)
         @unless($link->id === $group->root_item_id)
-        <article class="d-inline-block border rounded me-1 mb-1 pe-1" title="{{ $link->linkable->name }}{{ $link->linkable->sku ? " [SKU: {$link->linkable->sku}]" : '' }}">
-            <img src="{{ $link->linkable->getThumbnailUrl() }}" class="rounded-start" style="height: 2rem" />
-            <span class="fw-semibold me-1">{{ Str::limit($link->linkable->name, 12) }}</span>
-        </article>
+            <a class="d-inline-block border rounded me-1 mb-1 pe-1"
+               title="{{ $link->linkable->name }}{{ $link->linkable->sku ? " [SKU: {$link->linkable->sku}]" : '' }}"
+               href="{{ admin_link_to($link->linkable) }}"
+            >
+                <img src="{{ $link->linkable->getThumbnailUrl() }}" class="rounded-start" style="height: 2rem" />
+                <span class="fw-semibold me-1">{{ Str::limit($link->linkable->name, 12) }}</span>
+            </a>
         @endunless
     @endforeach
 @else
