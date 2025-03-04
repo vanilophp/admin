9b4eb89f2f86033e85fcd6175ec60406683697b6
Ervin Molnar <58359015+molnarerwin@users.noreply.github.com>
2025-03-04 12:38:57 +0200

    Aligned the ETA fields next to each-other

--- src/resources/views/shipping-method/_form.blade.php
+++ src/resources/views/shipping-method/_form.blade.php
@@ -4,15 +4,15 @@
             {!! icon('shipping') !!}
         </span>
         <x-appshell::floating-label :label="__('Name')" :is-invalid="$errors->has('name')">
-        {{ Form::text('name', null, [
-                'class' => 'form-control form-control-lg' . ($errors->has('name') ? ' is-invalid' : ''),
-                'placeholder' => __('Name of shipping method')
-            ])
-        }}
+            {{ Form::text('name', null, [
+                    'class' => 'form-control form-control-lg' . ($errors->has('name') ? ' is-invalid' : ''),
+                    'placeholder' => __('Name of shipping method')
+                ])
+            }}
         </x-appshell::floating-label>
     </div>
     @if ($errors->has('name'))
-        <input hidden class="form-control is-invalid" />
+        <input hidden class="form-control is-invalid"/>
         <div class="invalid-feedback">{{ $errors->first('name') }}</div>
     @endif
 </div>
@@ -84,48 +84,53 @@
 </div>
 
 <x-vanilo::configuration :model="$shippingMethod" reload-on-change-of="calculatorSelector" pass-on-reload="calculator"
-     :sample-refresh-route="route('vanilo.admin.shipping-method.create')"></x-vanilo::configuration>
+                         :sample-refresh-route="route('vanilo.admin.shipping-method.create')"></x-vanilo::configuration>
 
 <hr>
 
 <div class="mb-3 row">
-    <label for="eta_min" class="col-form-label col-form-label-sm col-md-2">{{ __('Minimum ETA') }}</label>
-    <div class="col-md-10">
-        {{ Form::number('eta_min', null, [
-                'class' => 'form-control form-control-sm' . ($errors->has('eta_min') ? ' is-invalid': ''),
-                'placeholder' => __('--')
-           ])
-        }}
-        @if ($errors->has('eta_min'))
-            <div class="invalid-feedback">{{ $errors->first('eta_min') }}</div>
-        @endif
+    <div class="col-md-6 col-xl-4">
+        <label for="eta_min" class="col-form-label col-form-label-sm col-md-2">{{ __('Minimum ETA') }}</label>
+        <div>
+            {{ Form::number('eta_min', null, [
+                    'class' => 'form-control form-control-sm' . ($errors->has('eta_min') ? ' is-invalid': ''),
+                    'id' => 'eta_min',
+                    'placeholder' => __('--')
+               ])
+            }}
+            @if ($errors->has('eta_min'))
+                <div class="invalid-feedback">{{ $errors->first('eta_min') }}</div>
+            @endif
+        </div>
     </div>
-</div>
 
-<div class="mb-3 row">
-    <label for="eta_max" class="col-form-label col-form-label-sm col-md-2">{{ __('Maximum ETA') }}</label>
-    <div class="col-md-10">
-        {{ Form::number('eta_max', null, [
-                'class' => 'form-control form-control-sm' . ($errors->has('eta_max') ? ' is-invalid': ''),
-                'placeholder' => __('--')
-           ])
-        }}
-        @if ($errors->has('eta_max'))
-            <div class="invalid-feedback">{{ $errors->first('eta_max') }}</div>
-        @endif
+    <div class="col-md-6 col-xl-4">
+        <label for="eta_max" class="col-form-label col-form-label-sm col-md-2">{{ __('Maximum ETA') }}</label>
+        <div>
+            {{ Form::number('eta_max', null, [
+                    'class' => 'form-control form-control-sm' . ($errors->has('eta_max') ? ' is-invalid': ''),
+                    'id' => 'eta_max',
+                    'placeholder' => __('--')
+               ])
+            }}
+            @if ($errors->has('eta_max'))
+                <div class="invalid-feedback">{{ $errors->first('eta_max') }}</div>
+            @endif
+        </div>
     </div>
-</div>
 
-<div class="mb-3 row">
-    <label for="eta_units" class="col-form-label col-form-label-sm col-md-2">{{ __('ETA Units') }}</label>
-    <div class="col-md-10">
-        {{ Form::select('eta_units', collect($timeUnits)->pluck('value', 'value'), null, [
-                'class' => 'form-select form-select-sm' . ($errors->has('eta_units') ? ' is-invalid': ''),
-                'placeholder' => __('--')
-           ])
-        }}
-        @if ($errors->has('eta_units'))
-            <div class="invalid-feedback">{{ $errors->first('eta_units') }}</div>
-        @endif
+    <div class="col-md-6 col-xl-4">
+        <label for="eta_units" class="col-form-label col-form-label-sm col-md-2">{{ __('ETA Units') }}</label>
+        <div>
+            {{ Form::select('eta_units', collect($timeUnits)->pluck('value', 'value'), null, [
+                    'class' => 'form-select form-select-sm' . ($errors->has('eta_units') ? ' is-invalid': ''),
+                    'id' => 'eta_units',
+                    'placeholder' => __('--')
+               ])
+            }}
+            @if ($errors->has('eta_units'))
+                <div class="invalid-feedback">{{ $errors->first('eta_units') }}</div>
+            @endif
+        </div>
     </div>
 </div>
