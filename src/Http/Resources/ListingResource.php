8d4365620eccc2f5dbe92e546e7dd692455974e3
Lajos Fazakas <lajos@artkonekt.com>
2025-06-18 14:41:46 +0300

    Simplify morph type resolution

--- src/Http/Resources/ListingResource.php
+++ src/Http/Resources/ListingResource.php
@@ -6,24 +6,17 @@ namespace Vanilo\Admin\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Vanilo\Contracts\Buyable;
 
 class ListingResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-        if ($this->resource instanceof Buyable) {
-            $morphTypeName = $this->resource->morphTypeName();
-        } else {
-            $morphTypeName = shorten($this->resource::class);
-        }
-
         return [
             'id' => $this->id,
             'name' => $this->resource->name,
             'price' => $this->resource->getPrice(),
             'thumbnail' => $this->resource->getThumbnailUrl(),
-            'morph_type_name' => $morphTypeName,
+            'morph_type_name' => morph_type_of($this->resource),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
