e5594c0fcd7876fca4d753d47d7e2cdc5267f565
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2025-06-25 15:00:37 +0300

    Fix inexistent icon usage

--- src/resources/views/components/name-lookup.blade.php
+++ src/resources/views/components/name-lookup.blade.php
@@ -3,7 +3,7 @@
         <label class="form-control-label mb-2">{{ $nameLabel ?? __('Product') }}</label>
         <div class="input-group">
                 <span class="input-group-text">
-                    {!! icon('name') !!}
+                    {!! icon('label') !!}
                 </span>
             {{ Form::text('name', null, [
                     'class' => 'form-control',
