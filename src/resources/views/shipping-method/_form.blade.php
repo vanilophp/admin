f928e17ff7079d23a6a95425c9ce6c3a7fe1196a
Lajos Fazakas <lajos@artkonekt.com>
2025-06-26 13:20:37 +0300

    Filter out UNUSED option from the form & simplify code by using @json directive
    Co-authored-by: Attila Fulop <1162360+fulopattila122@users.noreply.github.com>

--- src/resources/views/shipping-method/_form.blade.php
+++ src/resources/views/shipping-method/_form.blade.php
@@ -87,10 +87,12 @@
     <label class="col-form-label col-form-label-sm col-md-2">{{ __('Match Products') }}</label>
     <div class="col-md-10">
         @foreach($shippingCategoryMatchingConditions as $key => $value)
+            @unless(is_null($key))
             <div class="form-check form-check-inline {{ $errors->has('shipping_category_matching_condition') ? 'is-invalid' : '' }}">
                 {{ Form::radio('shipping_category_matching_condition', $key, $shippingMethod->shipping_category_matching_condition && $shippingMethod->shipping_category_matching_condition->value() == $key, ['id' => "shipping_category_matching_condition_$key", 'x-model' => 'shippingCategoryMatchingCondition', 'class' => 'form-check-input' . ($errors->has('shipping_category_matching_condition') ? ' is-invalid': '')]) }}
                 <label class="form-check-label" for="shipping_category_matching_condition_{{ $key }}">{{ $value }}</label>
             </div>
+            @endunless
         @endforeach
 
         @if ($errors->has('shipping_category_matching_condition'))
@@ -174,8 +176,8 @@
     <script>
         document.addEventListener('alpine:init', function() {
             Alpine.data('shippingMethod', () => ({
-                shippingCategoryMatchingCondition: '{{ old('shipping_category_matching_condition') ?: ($shippingMethod->shipping_category_matching_condition ? $shippingMethod->shipping_category_matching_condition->value() : 'none') }}',
-                selectedShippingCategory: '{{ old('shipping_category_id') ?: ($shippingMethod->shipping_category_id ?? '') }}',
+                shippingCategoryMatchingCondition: @json(old('shipping_category_matching_condition') ?: $shippingMethod->shipping_category_matching_condition?->value()),
+                selectedShippingCategory: @json(old('shipping_category_id') ?: $shippingMethod->shipping_category_id),
                 isCategorySelected() {
                     return this.selectedShippingCategory !== '';
                 }
